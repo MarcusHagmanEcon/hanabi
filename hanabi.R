559eea20897cb5de41aeca6c6cc5025716957081
Marcus Hagman <marcus.hagman@phdstudent.hhs.se>
2023-02-06 09:55:28 +0100

    Init hanabi project

---
+++ hanabi.R
@@ -0,0 +1,111 @@
+rm(list = ls(all.names = TRUE))
+library(dplyr)
+source('/Users/Bella/Desktop/hanabi_strategies.R')
+
+
+n_players <- 3
+n_hand <- 5
+moves_remaining <- n_players+1
+points = c()
+# Create deck:
+color <- rep(c('W','R','G','B','Y'),each=10)
+number <- rep(c(1,1,1,2,2,3,3,4,4,5),times=5)
+deck <- data.frame(color,number)
+
+playgame <- function(seed,strategy){
+  set.seed(seed)
+  lastround <- FALSE
+  # Initialize game:
+  player <- 1
+  clock <- 8
+  fuse <- 4
+  drawpile <- deck[sample(1:nrow(deck)), ]
+  rownames(drawpile) <- 1:nrow(drawpile)
+  
+  # Hand out cards:
+  hands <- drawpile[1:(n_players*n_hand),]
+  hands$player <- rep(1:n_players, each = n_hand)
+  drawpile <- drawpile[(n_players*n_hand+1):nrow(drawpile),]
+  
+  # Initialize info set, discard pile, table
+  info <- setNames(data.frame(matrix(ncol = 3, nrow = (n_players*n_hand))),c('color','number','player'))
+  info$player <- rep(1:n_players, each = n_hand)
+  table <- setNames(data.frame(matrix(0,ncol = 5, nrow = 1)),c('W','R','G','B','Y'))
+  discard <- setNames(data.frame(matrix(ncol = 3, nrow =0)),c('color','number','player'))
+  counter <- 0 
+  playing = TRUE
+  while (playing == TRUE) {
+    action <- strategy(player, clock, fuse, table, discard, hands[hands$player != player,], info, lastround)
+    if(action[1] == 'R'){         # Reveal info
+      if(clock == 0){
+        print('Illegal move: clock tokens are used up.')
+        break
+      }
+      clock = clock-1
+      if(action[3]=='C'){
+        info$color[info$player == action[2] & hands$color == action[4]] = action[4]
+      } else {
+        info$number[info$player == action[2] & hands$number == action[4]] = action[4]
+      }
+    } else if(action[1] == 'D'){  # Discard card
+      ind_disc <- strtoi(rownames(hands[hands$player==player,])[strtoi(action[2])])
+      discard <- rbind(discard, hands[ind_disc,])
+      hands <- hands[-ind_disc,]
+      info <- info[-ind_disc,]
+      if(lastround==FALSE){
+        hands <- rbind(hands,c(drawpile[1,],'player' = player))
+        drawpile <- drawpile[-1,]
+      } else{
+          hands <- rbind(hands,c(NA,NA,player))
+      }
+      info <- rbind(info,c(NA,NA,player))
+      rownames(hands) <- 1:nrow(hands)
+      rownames(info) <- 1:nrow(info)
+      clock <- clock +1
+    } else {                      # Play card
+      ind_play <- strtoi(rownames(hands[hands$player==player,])[strtoi(action[2])])
+      card <- hands[ind_play,]
+      hands <- hands[-ind_play,]
+      info <- info[-ind_play,]
+      if(lastround==FALSE){
+        hands <- rbind(hands,c(drawpile[1,],'player' = player))
+        drawpile <- drawpile[-1,]
+      } else{
+        hands <- rbind(hands,c(NA,NA,player))
+      }
+      info <- rbind(info,c(NA,NA,player))
+      rownames(hands) <- 1:nrow(hands)
+      rownames(info) <- 1:nrow(info)
+      if(table[,card$color]==card$number-1){
+        table[,card$color] = table[,card$color] +1
+      } else {
+        fuse <- fuse-1
+        discard <- rbind(discard, card)
+      }
+    }
+    player <- player + 1
+    if(player>3){
+      player <- player - 3
+    }
+    if(nrow(drawpile)==0){
+      moves_remaining <- moves_remaining-1
+      lastround = TRUE
+    }
+    if(moves_remaining==0){
+      playing = FALSE
+    }
+    if(nrow(drawpile)>0){
+      rownames(drawpile) <- 1:nrow(drawpile)
+    }
+    counter <- counter+1
+    #print(counter)
+  }
+  # points = append(points,sum(table))
+  # print(points)
+  return(sum(table))
+}
+
+for(i in 1:100){
+  points = append(points,playgame(i,strategy1))
+  print(i)
+}
---
+++ hanabi_strategies.R
@@ -0,0 +1,113 @@
+rm(list = ls())
+
+library(dplyr)
+
+# Outputs column number, from color letter
+color_column <- function(col){
+  if (is.na(col)){
+    return(NA)
+  } else if (col == "W"){
+    return(1)
+  } else if (col == "R" ){
+    return(2)
+  } else if (col == "G" ){
+    return(3)
+  } else if (col == "B" ){
+    return(4)
+  } else if (col == "Y" ){
+    return(5)
+  } else {
+    println("INVALID COLOR!")
+    return(6)
+  }
+}
+
+# Returns 0 if discardable, 1 if playable, 2 if not playable, 3 if unknown
+card_playable <- function(card_info, table){
+  if(is.na(card_info[2])){
+    return(3)
+  }
+  number = as.integer(card_info[2])
+  color_num = color_column(card_info[1])
+  if(number <= min(table[1,] ) ){
+    return(0)
+  } else if(!is.na(color_num) &&
+    table[1, color_num] >= number ){
+    return(0)
+  } else if(min(table[1,]) == max(table[1,]) && number == max(table[1,]) + 1 ){
+    return(1)
+  } else if(!is.na(color_num) && number == table[1,color_num] + 1){
+    return(1)
+  } else if(number > max(table[1,]) + 1){
+    return(2)
+  } else if(!is.na(color_num) && number > table[1,color_num] + 1){
+    return(2)
+  } else {
+    return(3)
+  }
+}
+
+# Outputs a five element string, each indicating the playability of each card
+hand_playable <- function(hand_info, table) {
+  result = c()
+  for(i in 1:nrow(hand_info)){
+    result <- append(result, card_playable(hand_info[i,], table) )
+  }
+  return(result)
+}
+
+strategy1 <- function(active_player, clock, fuse, table, discard, hands, info, last_round) {
+  own_hand_info <- info %>% filter(player == active_player) %>% select(color, number)
+  others_unknown_playable <- hand_playable(hands[,1:2],table)
+  others_info <- info %>% filter(player != active_player)
+  others_known_playable <- hand_playable(others_info[,1:2],table)
+  if (1 %in% hand_playable(own_hand_info, table) ){
+    #print('A')
+    return( c("P", which(hand_playable(own_hand_info, table) == 1)[1]) )
+  } else if (0 %in% hand_playable(own_hand_info, table) && clock < 8) {
+    #print('B')
+    return( c("D", which(hand_playable(own_hand_info, table) == 0)[1]) )
+  } else if (clock > 0 & TRUE %in% (others_known_playable == 3 & others_unknown_playable == 1)) {
+    #print('C')
+    reveal_index <- which((others_known_playable == 3 & 
+                                        others_unknown_playable == 1) == TRUE)[1]
+    reveal_player <- hands$player[reveal_index]
+    if(is.na(others_info$number[reveal_index])){
+      reveal_categ <- "N"
+      reveal_info <- hands$number[reveal_index]
+    } else { 
+      reveal_categ <- "C"
+      reveal_info <- hands$color[reveal_index]
+    }
+    reveal_player_index <- sum( hands$player[1:reveal_index] == reveal_player)
+    return( c("R", reveal_player, reveal_categ, reveal_info) )
+    
+  } else {
+    return( c("D", 1))
+  }
+  
+}
+# EXAMPLE
+
+# table <- data.frame( W = 3, R = 1, Y = 1, G = 1, B = 2)
+# 
+# hands <- data.frame(color = 
+#   c("Y", "W", "R", "W", "B",
+#     "B", "B", "G", "Y", "W"), 
+#   number = 
+#   c(1, 3, 5, 1, 2,
+#     4, 2, 2, 1, 3),
+#   player = rep(c(1,2), each = 5) )
+# 
+# info <- data.frame(color = 
+#   c(NA, NA, NA, "W", "B",
+#     NA, NA, NA, NA, "W",
+#     NA, "R", "W", "W", NA), 
+#   number = 
+#   c(1, 3, NA, 1, NA,
+#     4, 2, NA, 1, NA,
+#     1, 5, 5, NA, NA),
+#   player = rep(c(1,2,3), each = 5) )
+# 
+# strategy1(3, 5,4,table,0,hands,info,0)
+# 
